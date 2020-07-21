2017fa712d6e923111fbceaf3a28bf84672409dc
ivrom879 <ivrom879@yandex.ru>
2020-07-21 23:41:15 +0300

    init

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all()->toArray();
+        return array_reverse($books);
+    }
+
+    public function add(Request $request)
+    {
+        $book = new Book([
+            'name' => $request->input('name'),
+            'author' => $request->input('author')
+        ]);
+        $book->save();
+
+        return response()->json('The book successfully added');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+        $book->update($request->all());
+
+        return response()->json('The book successfully updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        $book = Book::find($id);
+        $book->delete();
+
+        return response()->json('The book successfully deleted');
+    }
+}
